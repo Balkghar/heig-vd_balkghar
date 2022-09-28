2159c9602b7120f34bf107c931ebdc87a08047d8
Balkghar <hugo.germano8@gmail.com>
2022-09-28 10:12:18 +0200

    exercice fini

--- PRG1/02-Buffer/02_buffer.cpp
+++ PRG1/02-Buffer/02_buffer.cpp
@@ -20,23 +20,23 @@ int main() {
    char     s;
 
    // justifier ce que vaut le buffer après CHAQUNE des instructions
-   cout << "entrer des valeurs : ";                      //
+   cout << "entrer des valeurs : ";                      // affiche une phrase
    cin.ignore(numeric_limits<streamsize>::max(), '\n');  //ignore toutes les données entrées avant le retour à la ligne
    cout << endl;                                         //fait un retour à la ligne
    cin >> a;                                             //stock la valeur entré par l'utilisateur dans a
    cin >> b;                                             //stock la valeur entré par l'utilisateur dans b
-   cin.ignore(numeric_limits<streamsize>::max(), '\n');  //ignore toutes les données entrées avant le retour à la ligne
+   cin.ignore(numeric_limits<streamsize>::max(), '\n');  //ignore toutes les données entrées avant le retour à la ligne et vide le buffer
    cin >> x;                                             //stock la valeur entré par l'utilisateur dans x
    cin >> y;                                             //stock la valeur entré par l'utilisateur dans y
-   cout << "voulez-vous sauver [o/n] : ";                
-   cin >> s;
-   cout << "sauver : " << s << endl;
-   cin >> a;
-   cin >> b;
-   cin >> x;
+   cout << "voulez-vous sauver [o/n] : ";                //affiche à l'utilisateur s'il veut sauvegarder
+   cin >> s;                                             //stock la valeur entré par l'utilisateur dans s
+   cout << "sauver : " << s << endl;                     //affiche la valeur que l'utilisateur a mis
+   cin >> a;                                             //stock la valeur entré par l'utilisateur dans a
+   cin >> b;                                             //stock la valeur entré par l'utilisateur dans b
+   cin >> x;                                             //stock la valeur entré par l'utilisateur dans x
    
    // fin de programme
-   cout << "Presser ENTER pour quitter";
-   cin.ignore(numeric_limits<streamsize>::max(), '\n');
-   return EXIT_SUCCESS;
+   cout << "Presser ENTER pour quitter";                 //affiche un string à l'utilisateur
+   cin.ignore(numeric_limits<streamsize>::max(), '\n');  //igonore la valeur entré par l'utilisatuer
+   return EXIT_SUCCESS;                                  //retrn un code de succès
 }
