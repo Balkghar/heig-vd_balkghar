272c12ddb6c6e1b1768277ccc01480fc4aaef524
Balkghar <hugo.germano8@gmail.com>
2022-10-12 14:42:11 +0200

    modificatoon

---
+++ PRG1/0-Exerices/1-3.8/main.cpp
@@ -0,0 +1,25 @@
+//---------------------------------------------------------
+// Fichier      : main.cpp
+// Auteur       : Hugo Germano
+// Date         : 12.10.2022
+
+// But          : Exercice 3.8
+//
+// Remarque     : 
+//
+// Modifs       :
+//
+// Compilateur  : 
+//---------------------------------------------------------
+
+#include <cstdlib>      // EXIT_SUCCESS
+#include <iostream>     // cout et cin
+#include <limits>       // numeric_limits<streamsize>
+
+using namespace std;
+int main() {
+    enum dayByMonth{january=1, february=10, march=1, april=2, may=2, june=3, jully=1, august=1, september=1, october=1, november=2, december=1};
+
+
+   return EXIT_SUCCESS;                                  //retrn un code de succès
+}
