e2dbe18fdc2fdfa7a6d1c28734066ec2feeef224
Balkghar <hugo.germano8@gmail.com>
2022-10-23 13:16:15 +0200

    Exercice supplémentaires

---
+++ PRG1/0-Exerices/3.28.cpp
@@ -0,0 +1,43 @@
+//---------------------------------------------------------
+// Fichier      : 3.28.cpp
+// Auteur       : Hugo Germano
+// Date         : 23.10.2022
+
+// But          : Exercice 3.28
+//
+// Remarque     : 
+//
+// Modifs       :
+//
+// Compilateur  : gcc (Ubuntu 11.2.0-19ubuntu1) 11.2.0
+//---------------------------------------------------------
+
+#include <cstdlib>      // EXIT_SUCCESS
+#include <iostream>     // cout et cin
+#include <iomanip>
+#include <limits>       // numeric_limits<streamsize>
+
+#define VIDER_BUFFER cin.ignore(numeric_limits<streamsize>::max(),'\n')
+
+using namespace std;
+int main() {
+   //déclaration des variables
+   int n;
+   double reponse = 1;
+
+   do{
+      cout << "Veuillez entrer n : ";
+      cin >> n;
+      VIDER_BUFFER;
+
+   }while(n < 0);
+   for (int i = 2; i <= n; ++i){
+      cout << reponse << "/" << 1/double(i) << endl;
+      reponse = reponse + 1/double(i);
+   }
+   cout << "La somme des " << n << " premiers termes de la serie vaut " << reponse << endl;
+   
+   cout << "Entrez ENTER pour quitter.";
+   VIDER_BUFFER;
+   return EXIT_SUCCESS;                                  //retourne un code de succès
+}
