c04642863040986f3cef6c3c0931d3b34ecf10fb
Balkghar <hugo.germano8@gmail.com>
2023-03-23 16:21:04 +0100

    update du 1.13

--- PRG2/1/1.13.c
+++ PRG2/1/1.13.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #define TAILLE_NOM 20
 int main(void) {
 
-   char nom[TAILLE_NOM + 1];
+   char nom[TAILLE_NOM];
    printf("Entrez votre nom (%d caract. max) : ", TAILLE_NOM);
-   
-   scanf("%20s", nom);
+
+   fgets(nom, TAILLE_NOM, stdin);
+
+   nom[strlen(nom)-1]='\0';
+
    printf("Votre nom est \"%s\"\n", nom);
    getchar();
    return EXIT_SUCCESS;
